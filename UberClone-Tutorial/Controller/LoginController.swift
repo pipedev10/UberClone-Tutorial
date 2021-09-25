cb3b16c12c0558e034ba5f0e89c2d19e338cc86f
Felipe Carrasco <felipe.carrasco@codelegends.cl>
2021-09-25 20:05:05 -0300

    11 firebase login user

--- UberClone-Tutorial/Controller/LoginController.swift
+++ UberClone-Tutorial/Controller/LoginController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginController: UIViewController {
     
@@ -43,6 +44,7 @@ class LoginController: UIViewController {
         let button = AuthButton(type: .system)
         button.setTitle("Log In", for: .normal)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
         return button
     }()
     
@@ -69,6 +71,20 @@ class LoginController: UIViewController {
     @objc func handleShowSignUp(){
         let controller = SignUpController()
         navigationController?.pushViewController(controller, animated: true)
+    }
+    
+    @objc func handleLogin(){
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            if let error = error {
+                print("There was an error in login \(error.localizedDescription)")
+                return
+            }
+            
+            print("successfully logged user in...")
+        }
+        print("12232")
     }
     
     // MARK: - Helper Functions
