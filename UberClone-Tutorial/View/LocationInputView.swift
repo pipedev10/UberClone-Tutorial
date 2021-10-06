249d8684eff440287a4193fc8aa5286433d9bc99
Felipe Carrasco <felipe.carrasco@codelegends.cl>
2021-10-05 23:07:40 -0300

    learn location input view wrap up

--- UberClone-Tutorial/View/LocationInputView.swift
+++ UberClone-Tutorial/View/LocationInputView.swift
@@ -12,8 +12,8 @@ protocol LocationInputViewDelegate: AnyObject {
 }
 
 class LocationInputView: UIView {
-
-   // MARK: - Properties
+    
+    // MARK: - Properties
     
     weak var delegate: LocationInputViewDelegate?
     
@@ -22,6 +22,62 @@ class LocationInputView: UIView {
         button.setImage(UIImage(named: "baseline_arrow_back_black_36dp")?.withRenderingMode(.alwaysOriginal), for: .normal)
         button.addTarget(self, action: #selector(handleBackTapped), for: .touchUpInside)
         return button
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Felipe Carrasco"
+        label.textColor = .darkGray
+        label.font = UIFont.systemFont(ofSize: 16)
+        return label
+    }()
+    
+    private let startLocationIndicatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        return view
+    }()
+    
+    private let linkingView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .darkGray
+        return view
+    }()
+    
+    private let destinationIndicatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        return view
+    }()
+    
+    private lazy var startLocationTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Current Location"
+        tf.backgroundColor = .systemGray6
+        tf.isEnabled = false
+        tf.font = UIFont.systemFont(ofSize: 14)
+        
+        let paddingView = UIView()
+        paddingView.setDimensions(height: 30, width: 8)
+        tf.leftView = paddingView
+        tf.leftViewMode = .always
+        
+        return tf
+    }()
+    
+    private lazy var destinationLocationTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Enter a destination..."
+        tf.backgroundColor = .lightGray
+        tf.returnKeyType = .search
+        tf.font = UIFont.systemFont(ofSize: 14)
+        
+        let paddingView = UIView()
+        paddingView.setDimensions(height: 30, width: 8)
+        tf.leftView = paddingView
+        tf.leftViewMode = .always
+        
+        return tf
     }()
     
     // MARK: - LifeCycle
@@ -35,6 +91,29 @@ class LocationInputView: UIView {
         
         addSubview(backButton)
         backButton.anchor(top: topAnchor, left: leftAnchor, paddingTop: 44, paddingLeft: 12, width: 24, height: 25)
+        
+        addSubview(titleLabel)
+        titleLabel.centerY(inView: backButton)
+        titleLabel.centerX(inView: self)
+        
+        addSubview(startLocationTextField)
+        startLocationTextField.anchor(top: backButton.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 4, paddingLeft: 40, paddingRight: 40, height: 30)
+        
+        addSubview(destinationLocationTextField)
+        destinationLocationTextField.anchor(top: startLocationTextField.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 12, paddingLeft: 40, paddingRight: 40, height: 30)
+        
+        addSubview(startLocationIndicatorView)
+        startLocationIndicatorView.centerY(inView: startLocationTextField, leftAnchor: leftAnchor, paddingLeft: 20)
+        startLocationIndicatorView.setDimensions(height: 6, width: 6)
+        startLocationIndicatorView.layer.cornerRadius = 6 / 2
+        
+        addSubview(destinationIndicatorView)
+        destinationIndicatorView.centerY(inView: destinationLocationTextField, leftAnchor: leftAnchor, paddingLeft: 20)
+        destinationIndicatorView.setDimensions(height: 6, width: 6)
+        
+        addSubview(linkingView)
+        linkingView.centerX(inView: startLocationIndicatorView)
+        linkingView.anchor(top: startLocationIndicatorView.bottomAnchor, bottom: destinationIndicatorView.topAnchor, paddingTop: 4, paddingBottom: 4, width: 0.5)
     }
     
     required init?(coder: NSCoder) {
