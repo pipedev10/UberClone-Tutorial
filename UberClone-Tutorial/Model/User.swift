807eaf184be1f7abfa31c8a7bdfb8fa3c8c8b4b7
Felipe Carrasco <felipe.carrasco@codelegends.cl>
2021-10-16 22:27:44 -0300

    27 feature: fetch drivers nearby from my location

--- UberClone-Tutorial/Model/User.swift
+++ UberClone-Tutorial/Model/User.swift
@@ -5,10 +5,13 @@
 //  Created by Pipe Carrasco on 11-10-21.
 //
 
+import CoreLocation
+
 struct User {
     let fullname: String
     let email: String
     let accountType: Int
+    var location: CLLocation?
     
     init(dictionary: [String: Any]) {
         self.fullname = dictionary["fullname"] as? String ?? ""
--- UberClone-Tutorial/Service/Service.swift
+++ UberClone-Tutorial/Service/Service.swift
@@ -6,6 +6,8 @@
 //
 
 import Firebase
+import CoreLocation
+import GeoFire
 
 let DB_REF = Database.database().reference()
 let REF_USERS = DB_REF.child("users")
@@ -15,15 +17,26 @@ struct Service {
     
     static let shared = Service()
     
-    
-    func fetchUserData(completion: @escaping(User) -> Void){
-        guard let currentUid = Auth.auth().currentUser?.uid else { return }
-        REF_USERS.child(currentUid).observeSingleEvent(of: .value) { (snapshot) in
+    func fetchUserData(uid: String, completion: @escaping(User) -> Void){
+        REF_USERS.child(uid).observeSingleEvent(of: .value) { (snapshot) in
             guard let dictionary = snapshot.value as? [String: Any] else { return }
             let user = User(dictionary: dictionary)
             completion(user)
         }
     }
     
+    func fetchDrivers(location: CLLocation, completion: @escaping(User) -> Void){
+        let geofire = GeoFire(firebaseRef: REF_DRIVER_LOCATIONS)
+        
+        REF_DRIVER_LOCATIONS.observe(.value) { (snapshot) in
+            geofire.query(at: location, withRadius: 50).observe(.keyEntered, with: { (uid, location) in
+                self.fetchUserData(uid: uid) { (user) in
+                    var driver = user
+                    driver.location = location
+                    completion(driver)
+                }
+            })
+        }
+    }
     
 }
