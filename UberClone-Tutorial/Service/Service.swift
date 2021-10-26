f86c901df186ede1b52b6096dbfc46f004c8b616
Felipe Carrasco <felipe.carrasco@codelegends.cl>
2021-10-26 19:45:18 -0300

    43 feature: requesting ride

--- UberClone-Tutorial/Service/Service.swift
+++ UberClone-Tutorial/Service/Service.swift
@@ -12,6 +12,7 @@ import GeoFire
 let DB_REF = Database.database().reference()
 let REF_USERS = DB_REF.child("users")
 let REF_DRIVER_LOCATIONS = DB_REF.child("driver-locations")
+let REF_TRIPS = DB_REF.child("trips")
 
 struct Service {
     
@@ -40,4 +41,15 @@ struct Service {
         }
     }
     
+    func uploadTrip(_ pickupCoordinates: CLLocationCoordinate2D, _ destinationCoordinates: CLLocationCoordinate2D, completion: @escaping(Error?, DatabaseReference) -> Void){
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        let pickupArray = [pickupCoordinates.latitude, pickupCoordinates.longitude]
+        let destionationArray = [destinationCoordinates.latitude, destinationCoordinates.longitude]
+        
+        let values = ["pickupCoordinates": pickupArray,
+                      "destinationCoordinates": destionationArray]
+        
+        REF_TRIPS.child(uid).updateChildValues(values, withCompletionBlock: completion)
+    }
 }
