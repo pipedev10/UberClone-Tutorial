cfe8a3232789cf03ec9ba8741fe07a244bab8f30
Felipe Carrasco <felipe.carrasco@codelegends.cl>
2021-11-05 14:14:54 -0300

    50 present loading view during trip

--- UberClone-Tutorial/Utils/Extensions.swift
+++ UberClone-Tutorial/Utils/Extensions.swift
@@ -160,3 +160,53 @@ extension MKMapView {
         setVisibleMapRect(zoomRect, edgePadding: insets, animated: true)
     }
 }
+
+
+extension UIViewController {
+    func shouldPresentLoadingView(_ present: Bool, message: String? = nil){
+        if present {
+            let loadingView = UIView()
+            loadingView.frame = self.view.frame
+            loadingView.backgroundColor = .black
+            loadingView.alpha = 0
+            loadingView.tag = 1
+            
+            let indicator = UIActivityIndicatorView()
+            indicator.style = .large
+            indicator.color = .white
+            indicator.center = loadingView.center
+            
+            let label = UILabel()
+            label.text = message
+            label.font = UIFont.systemFont(ofSize: 20)
+            label.textColor = .white
+            label.textAlignment = .center
+            label.alpha = 0.87
+            
+            self.view.addSubview(loadingView)
+            
+            view.addSubview(indicator)
+            view.addSubview(label)
+            
+            label.centerX(inView: view)
+            label.anchor(top: indicator.bottomAnchor, paddingTop: 32)
+            
+            indicator.startAnimating()
+            
+            UIView.animate(withDuration: 0.3) {
+                loadingView.alpha = 0.7
+            }
+        } else {
+            view.subviews.forEach { (subView) in
+                if subView.tag == 1 {
+                    UIView.animate(withDuration: 0.3) {
+                        subView.alpha = 0
+                    } completion: { _ in
+                        subView.removeFromSuperview()
+                    }
+
+                }
+            }
+        }
+    }
+}
