d9fd2722296179e19af8724cbe724619e538cc84
Felipe Carrasco <felipe.carrasco@codelegends.cl>
2021-12-10 17:58:08 -0300

    77 settings page ui populate with user

--- UberClone-Tutorial/Controller/SideMenu/ContainerController.swift
+++ UberClone-Tutorial/Controller/SideMenu/ContainerController.swift
@@ -164,7 +164,10 @@ extension ContainerController: MenuControllerDelegate {
             case .yourTrips:
                 break
             case .settings:
-                break
+                guard let user = self.user else { return }
+                let controller = SettingsController(user: user)
+                let nav = UINavigationController(rootViewController: controller)
+                self.present(nav, animated: true, completion: nil)
             case .logout:
                 let alert = UIAlertController(title: nil, message: "Are you want to log out?", preferredStyle: .actionSheet)
                 
